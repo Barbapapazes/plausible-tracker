959e352f503c926af5b9e0e69b8efa604ea99b40
Maxime Pauvert <maximepvrt@gmail.com>
2024-04-11 16:19:52 +0200

    feat: add `ignoredHostnames` allow exlude a root domain

--- src/event.ts
+++ src/event.ts
@@ -23,7 +23,7 @@ export function isIgnored(
   /** Hostnames to ignore */
   ignoredHostnames: string[],
 ): boolean {
-  return ignoredHostnames.includes(hostname)
+  return ignoredHostnames.some(ignoredHostname => hostname.endsWith(ignoredHostname));
 }
 
 /**
