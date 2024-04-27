3fe24bd0f5c2c2b66c6758772310306d8572ca89
barbapapazes <e.soubiran25@gmail.com>
2024-04-27 20:49:57 +0200

    refactor: make this optional to avoid potential breaking change

--- src/event.ts
+++ src/event.ts
@@ -22,8 +22,15 @@ export function isIgnored(
   hostname: string,
   /** Hostnames to ignore */
   ignoredHostnames: string[],
+  /** Ignore sub domain  */
+  ignoreSubDomains: boolean,
 ): boolean {
-  return ignoredHostnames.some(ignoredHostname => hostname === ignoredHostname || hostname.endsWith('.' + ignoredHostname))
+  if (ignoreSubDomains)
+    // Both `example.com` and `sub.example.com` will be ignored
+    return ignoredHostnames.some(ignoredHostname => hostname === ignoredHostname || hostname.endsWith(`.${ignoredHostname}`))
+
+  // Only `example.com` will be ignored
+  return ignoredHostnames.includes(hostname)
 }
 
 /**
